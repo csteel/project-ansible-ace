aee9bb6ba1b05439d6fc7349617a2f706a4be403
Christopher Steel <christopher.steel@mcgill.ca>
2016-08-25 15:29:40 -0400

    added roles directory and script to install roles

---
+++ roles/install_roles.sh
@@ -0,0 +1,10 @@
+# csteel repos
+git clone git@github.com:csteel/ansible-role-shorewall.git shorewall
+git clone git@github.com:csteel/ansible-role-rrp.git rrp
+git clone git@github.com:csteel/ansible-role-ensure_dirs.git ensure_dirs 
+# cjsteel repos
+git clone git@github.com:cjsteel/ansible-role-deployment_user.git deployment_user
+git clone git@github.com:cjsteel/ansible-role-virtualbox.git virtualbox
+git clone git@github.com:cjsteel/ansible-role-acemenu.git acemenu
+git clone git@github.com:cjsteel/ansible-role-vagrant.git vagrant
+
